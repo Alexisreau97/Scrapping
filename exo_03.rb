17e932bf2211a95a218c6e09188d5cb9ba2cbe87
MelloFlo <florian_pinto@outlook.fr>
2018-10-11 18:20:58 +0200

    modif exo 3

--- exo_03.rb
+++ exo_03.rb
@@ -7,9 +7,12 @@ def depute_name(url)
   name_array = []
 
   page = Nokogiri::HTML(open(url))
-  name = page.xpath('/html/body/div[3]/div/div/section/div/article/div[3]/div/div[3]/div[1]/ul[1]/li[1]/a').text
+  name = page.css('#deputes-list').select { |link| link['class'] == 'col3' }
 
   puts name
 end
 
 depute_name("http://www2.assemblee-nationale.fr/deputes/liste/alphabetique")
+
+
+#projet non fini
