ea70281b14afcb8e5f1368d94397bf5e41852aa7
Alexisreau97 <alexis.reaupro@gmail.com>
2018-10-11 12:57:37 +0200

    update

--- exo_01.rb
+++ exo_01.rb
@@ -4,8 +4,9 @@ require 'open-uri'
 require 'pry'
 
 def get_the_email_of_a_townhal_from_its_webpage(url)
-  page = Nokogiri::HTML(open(url))   
-  puts page.css('/html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]').text
+  page = Nokogiri::HTML(open(url))
+  page.css('.tr-last')[3].text.split(" ")[2]
+  puts page.xpath('/html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]').text
 end
 
 get_the_email_of_a_townhal_from_its_webpage("http://annuaire-des-mairies.com/95/vaureal.html")
@@ -13,7 +14,9 @@ get_the_email_of_a_townhal_from_its_webpage("http://annuaire-des-mairies.com/95/
 
 def get_all_the_urls_of_val_doise_townhalls(url)
   page = Nokogiri::HTML(open(url))   
-  puts page.css('a.lientxt')[1]['href']
+  links = page.css('a.lientxt')
+  links.each{ |i| puts "https://annuaire-des-mairies.com#{i['href'].delete_prefix(".")}"}
 end
 
 get_all_the_urls_of_val_doise_townhalls("http://annuaire-des-mairies.com/val-d-oise.html")
+
