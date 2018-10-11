41c6633963c77dc47d698b04520d8e0c8d61a99e
Alexisreau97 <alexis.reaupro@gmail.com>
2018-10-11 13:12:43 +0200

    perform

--- exo_01.rb
+++ exo_01.rb
@@ -9,8 +9,6 @@ def get_the_email_of_a_townhal_from_its_webpage(url)
   puts page.xpath('/html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]').text
 end
 
-get_the_email_of_a_townhal_from_its_webpage("http://annuaire-des-mairies.com/95/vaureal.html")
-
 
 def get_all_the_urls_of_val_doise_townhalls(url)
   page = Nokogiri::HTML(open(url))   
@@ -18,5 +16,10 @@ def get_all_the_urls_of_val_doise_townhalls(url)
   links.each{ |i| puts "https://annuaire-des-mairies.com#{i['href'].delete_prefix(".")}"}
 end
 
-get_all_the_urls_of_val_doise_townhalls("http://annuaire-des-mairies.com/val-d-oise.html")
 
+def perfom
+	get_the_email_of_a_townhal_from_its_webpage("http://annuaire-des-mairies.com/95/vaureal.html")
+	get_all_the_urls_of_val_doise_townhalls("http://annuaire-des-mairies.com/val-d-oise.html")
+end
+
+perfom
